474d01d768043c3d45b5b44fb11ca8c04c778970
Mārcis Zoldners <mzo@adevo.io>
2017-06-07 16:58:45 +0300

    initial commit

---
+++ src/app/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+    render() {
+        var user = {
+            name: "Anna",
+            hobbies: ["Sports"]
+        };
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offest-1">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offest-1">
+                        <Home name={"Maarc"} age={23} user={user}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
