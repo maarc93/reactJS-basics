26959e4d06587d22cd971eec4c768a37cf515816
Mārcis Zoldners <mzo@adevo.io>
2017-06-09 13:36:27 +0300

    Commit

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,12 +1,35 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            age: props.initialAge
+        };
+    }
+
+    onMakeOlder() {
+        this.setState({
+            age: this.state.age + 3
+        });
+    }
+
     render() {
         return(
             <div>
                 <p>In a new Component!</p>
-                <p>You name is {this.props.name}</p>
+                <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+                <hr/>
+                <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button>
+                <hr/>
+                <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
             </div>
         );
     }
 }
+
+Home.propTypes = {
+    name: React.PropTypes.string,
+    initialAge: React.PropTypes.number,
+    greet: React.PropTypes.func
+};
--- src/app/index.js
+++ src/app/index.js
@@ -1,26 +1,25 @@
 import React from "react";
-import { render } from "react-dom";
+import {render} from "react-dom";
 
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
 
 class App extends React.Component {
-    render() {
-        var user = {
-            name: "Anna",
-            hobbies: ["Sports"]
-        };
+    onGreet() {
+        alert("Hello!");
+    }
 
+    render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offest-1">
-                        <Header/>
+                        <Header homeLink="Home"/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offest-1">
-                        <Home name={"Maarc"} age={23} user={user}/>
+                        <Home name={"Maarc"} initialAge={23} greet={this.onGreet}/>
                     </div>
                 </div>
             </div>
