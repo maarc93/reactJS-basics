85fac81af78a224eb58188ca3fd3471e378c1d89
Mārcis Zoldners <mzo@adevo.io>
2017-06-13 00:41:51 +0300

    Passing Data between Parent and Child

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,7 +4,9 @@ export class Home extends React.Component {
     constructor(props) {
         super();
         this.state = {
-            age: props.initialAge
+            age: props.initialAge,
+            status: 0,
+            homeLink: "Changed Link"
         };
     }
 
@@ -12,6 +14,10 @@ export class Home extends React.Component {
         this.setState({
             age: this.state.age + 3
         });
+    }
+
+    onChangeLink() {
+        this.props.changeLink(this.state.homeLink);
     }
 
     render() {
@@ -23,6 +29,8 @@ export class Home extends React.Component {
                 <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button>
                 <hr/>
                 <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+                <hr/>
+                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
             </div>
         );
     }
